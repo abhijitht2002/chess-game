6637c87c302b91f9dac5448074bec9e951bf8718
abhijith <abhijitht210@gmail.com>
2025-08-26 14:23:24 +0530

    basic board, pieces created; click testing done

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// import { drawPiece } from "./board/board.js";
+import { initializeBoard } from "./board/board.js";
+// import { initializePieces } from "./board/board.js";
+// import { boardGenerator } from "./board/board.js";
+// import { drawBoard } from "./board/board.js";
+
+
+initializeBoard()
+
+// const board = boardGenerator(8)
+
+// initializePieces(board)
+
+// board[5][5] = "X"
+
+// console.log(board)
+
+// drawBoard()
+// drawPiece()
