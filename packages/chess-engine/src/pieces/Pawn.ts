5f6ce200fc2eb292e4f90ce9133e535b4f675593
abhijith <abhijitht210@gmail.com>
2026-09-07 23:39:09 +0530

    ✨ feat: add pawn move generation

--- packages/chess-engine/src/pieces/Pawn.ts
+++ packages/chess-engine/src/pieces/Pawn.ts
@@ -1,11 +1,42 @@
 // Pawn.ts
 
-import type { Position } from "../types/position.js";
+import type { Position } from "../board/Position.js";
 import { Piece } from "./Piece.js";
 
 export class Pawn extends Piece {
   // pawn-specific movement
+
   getMoves(): Position[] {
-    return [];
+    const dir = this.color === "white" ? -1 : 1;
+
+    let moves: Position[] = [];
+
+    // advance
+    moves.push({
+      file: this.position.file,
+      rank: this.position.rank + dir,
+    });
+
+    // double advance
+    const startRow = this.color === "white" ? 6 : 1;
+    if (this.position.file === startRow) {
+      moves.push({
+        file: this.position.file,
+        rank: this.position.rank + 2 * dir,
+      });
+    }
+
+    // capture
+    moves.push({
+      file: this.position.file + 1,
+      rank: this.position.rank + dir,
+    });
+
+    moves.push({
+      file: this.position.file - 1,
+      rank: this.position.rank + dir,
+    });
+
+    return moves;
   }
 }
